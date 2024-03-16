e0744fd77ebeb2db5194454bf6cbf96e9c0f3535
rupesh kumar <rupeshkumarrk004@gmail.com>
2024-03-16 15:24:40 +0530

    push code to github

---
+++ react-user-agent/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';  
+import UserAgentInfo from './UserAgentInfo';
+
+const App = () => {
+    return (
+        <div>
+            <UserAgentInfo />
+        </div>
+    );
+};
+
+export default App;
